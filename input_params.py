4ad678e46327c5620850c1f75f5a76a660dd6b64
hjaleta <hjaleta@gmail.com>
2021-10-07 22:32:03 +0200

    ye

--- input_params.py
+++ input_params.py
@@ -8,22 +8,22 @@ jef_terms = {"rho": False, "rho_t": False, "J": False, "J_t": True}
 # The region where the laser pulse hits the plates. Shape can be "circle" or "rectangle"
 # for circle, give radius in nm, for rectangle give side lengths x and y
 exc_region = {"shape": "circle", "radius": 500, "x_side": 1000, "y_side": 1000} 
-sim_params = {"dx":100, "dy":100, "dz":1, "dt": 1, # time is fs, length is nm
+sim_params = {"dx":1000, "dy":1000, "dz":1, "dt": 1, # time is fs, length is nm
         "jef_terms": jef_terms, "exc_region": exc_region} 
 medium_params = [
-    {"label": "Mag",
+    {"material":"Fe",
     "x": (-1000, 1000), "y":(-1000, 1000), "z": (0,6),
     "use_Jx": False, "use_Jy": False, "use_Jz": False, 
     "use_rho":False, 
     "use_field": False,
-    "material":"Fe", "theta":0},
+     "theta":0, "mu":0, "n":1},
 
-    {"label": "nonMag",
+    {"material":"Pt",
     "x": (-1000, 1000), "y":(-1000, 1000), "z": (6,11),
     "use_Jx": True, "use_Jy": False, "use_Jz": False, 
     "use_rho":False, 
     "use_field": False,
-    "material":"Pt", "theta":0.068}
+    "theta":0.068, "mu": 0, "n": 70}
     ]
 
 # z_points = np.array([1, 5, 10, 50, 100, 500, 1000, 5000, 10000, 50000, 100000]) * 100 # distances z for E(z,t) ranging between 0.1 um and 10 cm
