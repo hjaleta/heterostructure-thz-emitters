ae78de254b9796b19ff4e28e2487a7d32f214774
hjaleta <hjaleta@github.com>
2021-10-11 21:05:24 +0200

    fix

---
+++ pipeline_tools/build_simulation_input.py
@@ -0,0 +1,24 @@
+import os
+import re
+
+def build_input_params_py(z, param_original_path, param_target_path, spin_flux_path):
+    with open(param_original_path, 'r') as f:
+        lines = f.read().split("\n")        
+    lines[14] = f'"x": (-1000, 1000), "y":(-1000, 1000), "z": (0,{z[0]}),'
+    lines[21] = f'"x": (-1000, 1000), "y":(-1000, 1000), "z": ({z[0]}, {sum(z)}),'
+    lines[2] = f'spin_flux_path = "{spin_flux_path}"'
+    input_params_string = "\n".join(lines)
+    input_params_file = open(param_target_path,"w")
+    input_params_file.write(input_params_string)
+    input_params_file.close()
+
+def build_simulation_setup(parent_dir, new_parent_dir, param_original_path):
+    for folder in os.listdir(parent_dir):
+        folder_dir = "/".join([parent_dir, folder])
+        spin_flux_path = "/".join([folder_dir, "flux.out"])
+        nums = re.findall(r"[0-9]+", folder)
+        z = (int(nums[0]),int(nums[1]))
+        param_target_path = "/".join([folder_dir, "input_params.py"])
+        new_folder_dir = "/".join([new_parent_dir, folder])
+        os.mkdir(new_folder_dir)
+        build_input_params_py(z, param_original_path, param_target_path, spin_flux_path)
---
+++ pipeline_tools/check_data.py
@@ -0,0 +1,62 @@
+import os
+import numpy as np
+# import matplotlib.pyplot as plt
+import shutil
+
+def check_folders(folder_rootdir, filename):
+    N_folders = 0
+    corrupted_folders = []
+
+    for folder in os.listdir(folder_rootdir):
+        N_folders += 1
+        folderpath = folder_rootdir+folder
+        has_file = False
+        for file in os.listdir(folderpath):
+            if file == filename:
+                has_file = True
+        if not has_file:
+            corrupted_folders.append(folderpath)
+    
+    return corrupted_folders, N_folders
+
+def check_flux(folder_rootdir, expected_flux_time):#, remove, newdir):
+
+    complete = 0
+    complete_folders = []
+    incomplete_folders = []
+    
+    for folder in os.listdir(folder_rootdir):
+        folderpath = folder_rootdir+folder
+        for file in os.listdir(folderpath):
+            if file == "flux.out":
+                try:
+                    arr = np.loadtxt(folder_rootdir + "/" + folder + "/" + file)
+                    t = arr.shape[0]
+                    if t >= expected_flux_time:
+                        complete_folders.append(folder)
+                        complete += 1
+                    else:
+                        incomplete_folders.append(folder)
+                except:
+                    incomplete_folders.append(folder)
+    return complete_folders, incomplete_folders
+
+def delete_filetype(parent_dir, file_names:list):
+    file_death_note = []
+    for dirpath, dirs, files in os.walk(parent_dir):
+        for f in files:
+            for ext in file_names:
+                if f[-len(ext):] == ext:
+                    file_death_note.append("/".join([dirpath,file]))
+    for f in file_death_note:
+        os.remove(f)
+
+def delete_dirtype(parent_dir, dir_names:list):
+    dir_death_note = []
+    for dirpath, dirs, files in os.walk(parent_dir):
+        for d in dirs:
+            for ext in dir_names:
+                if d[-len(ext):] == ext:
+                    dir_death_note.append("/".join([dirpath,d]))
+    for d in dir_death_note:
+        shutil.rmtree(d)
