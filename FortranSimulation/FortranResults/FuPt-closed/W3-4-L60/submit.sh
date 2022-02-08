e4fed06b3c31d4dc18c270e5015e7f7928afa2a0
hjaleta <hjaleta@gmail.com>
2022-02-08 15:25:28 +0100

    remove cavhed

--- FortranSimulation/FortranResults/FuPt-closed/W3-4-L60/submit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -l
-#SBATCH -J W3-4-L60
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH -t 24:00:00
-#SBATCH -A snic2021-1-22
-
-srun /proj/oppeneer/users/x_hjali/src-V2.6/tetralith/main.e < input.dat> oput.dat
